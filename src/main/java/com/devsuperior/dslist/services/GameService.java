0c420ae92f01510c2b426c86b5b1fc7f38c2fb42
vilmarmoraes <vilmarpomifrai@hotmail.com>
2023-11-17 14:16:45 -0300

    GameService

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -28,6 +28,7 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+//		List<GameMinDTO> dto =  result.stream().map(x -> new GameMinDTO(x)).toList();
 		return result.stream().map(GameMinDTO::new).toList();
 	}
 	
